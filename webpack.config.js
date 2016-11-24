bf49e3dddb9bc16e9c43938f51224edee0af895f
Alex Coles <alex@alexbcoles.com>
2016-11-24 23:44:09 +0100

    Add, configure file- and url-loader for fonts
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,11 @@ module.exports = {
       {
         test:     require.resolve("jquery"),
         loader:   "expose-loader?$!expose-loader?jQuery"
-      }
+      },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
+      { test: /\.(woff|woff2)$/,            loader: "url-loader?prefix=font/&limit=5000" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
     ]
   },
   plugins: [
