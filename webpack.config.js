2c632218d67ec6c12cee1ff7fc10af3b60962ddf
Alex Coles <alex@alexbcoles.com>
2016-11-25 09:20:34 +0100

    Enable sourcemaps for JS
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     path:     path.join(__dirname, 'public', 'javascripts'),
     publicPath: '/javascripts/'
   },
+  devtool: 'sourcemap',
   module: {
     loaders: [
       {
